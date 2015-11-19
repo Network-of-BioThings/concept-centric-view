4dc04ce19d7fee51721de413d663c396680b98e9
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-11-19 14:12:44 -0800

    Add basic search field

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,1 +1,16 @@
+'use strict';
+
+var SearchController = function($rootScope, $scope, $routeParams, $location, ConceptService) {
+
+  $scope.query = "";
+
+  $scope.search = function() {
+
+    console.log($scope.query);
+  }
+
+}
+
+DashboardController.$inject = ["$rootScope", "$scope", "$routeParams", "$location", "ConceptService"];
+angularApp.controller('SearchController', SearchController);
 
