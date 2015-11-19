4c0e6f7008b51b5d969647c6ac43b5046d64ab47
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-11-19 12:57:11 -0800

    Add search page

--- app/scripts/app-config.js
+++ app/scripts/app-config.js
@@ -2,15 +2,19 @@
 /*global angularApp */
 'use strict';
 
-var angularConfig = function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+var angularConfig = function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/search.html',
+      controller: 'DashboardController'
+    })
+    .when('/dashboard/:query', {
+      templateUrl: 'views/dashboard.html',
+      controller: 'DashboardController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 };
 
 angularConfig.$inject = ['$routeProvider', '$locationProvider'];
