3f58d55f76155210c207f4fee7d419f148c9a1c2
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-11-19 18:12:13 -0800

    angular-jqcloud configuration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,4 +2,4 @@
 /*global angular */
 'use strict';
 
-var angularApp = angular.module('angularJsCcvApplication', ['ui.bootstrap', 'ngRoute', 'ngAnimate', "ngSanitize", "ui.select", "nvd3ChartDirectives", "dang-jssor", "d3"]);
+var angularApp = angular.module('angularJsCcvApplication', ['ui.bootstrap', 'ngRoute', 'ngAnimate', "ngSanitize", "ui.select", "nvd3ChartDirectives", "dang-jssor", "d3", "angular-jqcloud"]);
